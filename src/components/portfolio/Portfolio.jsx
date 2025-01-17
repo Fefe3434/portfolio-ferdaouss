af582aa48a3aa26be9c7ca25c054e95a70154893
Ferdaouss Adlani <adlani.ferdaous@gmail.com>
2025-01-17 13:41:44 +0100

    fix: new projects added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/car.png'
+import IMG1 from '../../assets/car-img-2.png'
+import IMG4 from '../../assets/car.png'
 import IMG2 from '../../assets/api5.png'
+import IMG3 from '../../assets/chat-room-img.png'
 
 
 const data = [
+
   {
     id: 1,
     image: IMG1,
-    title: 'Car Selling Plateform',
-    github: 'https://github.com/Fefe3434/car_hub',
+    title: 'Car Selling Plateform-FrontEnd',
+    github: ['https://github.com/Fefe3434/car_hub_frontend']
+    // demo: 'http://dribble.com'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Car Selling Plateform-BackEnd',
+    github: ['https://github.com/Fefe3434/car_hub']
     // demo: 'http://dribble.com'
   },
   {
@@ -17,6 +27,13 @@ const data = [
     image: IMG2,
     title: 'Articles API',
     github: 'https://github.com/Fefe3434/node_api_articles',
+    // demo: 'http://dribble.com'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Chat App',
+    github: 'https://github.com/Fefe3434/chat-room-react-node.js',
     // demo: 'http://dribble.com'
   },
 
@@ -39,7 +56,6 @@ const Portfolio = () => {
                 <h3>{title}</h3>
                 <div className="portfolio_item-cta">
                   <a href={github} className='btn' target='_blank'>Github</a>
-                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
                 </div>
               </article>
             )
